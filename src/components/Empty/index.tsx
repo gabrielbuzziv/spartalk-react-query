5591fa93a66c5945f851482c72abbdd65221d5de
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-02-04 07:13:09 -0300

    add empty component

---
+++ src/components/Empty/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+export function Empty () {
+  return (
+    <Container>
+      <Text>Não foram adicionada tarefas ainda.</Text>
+    </Container>
+  )
+}
