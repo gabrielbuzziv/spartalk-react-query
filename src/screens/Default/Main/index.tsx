57b5b8e4527cea8662d6961f4ec44c3da855320d
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-02-04 07:13:39 -0300

    add default example

--- src/screens/Default/Main/index.tsx
+++ src/screens/Default/Main/index.tsx
@@ -1,10 +1,88 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ListRenderItemInfo } from 'react-native';
+import { Create } from '../../../components/Create';
+import { Empty } from '../../../components/Empty';
+import { TodoItem } from '../../../components/TodoItem';
+import { Todo } from '../../../domains/todos';
+import { createTodoUseCase } from '../../../useCases/Todos/createTodoUseCase';
+import { getTodosListUseCase } from '../../../useCases/Todos/getTodosListUseCase';
+import { markAsDoneUseCase } from '../../../useCases/Todos/markAsDoneUseCase';
+import { Container, List, Title } from './styles';
 
-export function DefaultMain () {
+export function DefaultMain() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+
+  const [data, setData] = useState<Todo[]>([] as Todo[]);
+
+  const loadData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const listOfTodos = await getTodosListUseCase({ done: false });
+      setData(listOfTodos);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const refreshData = useCallback(async () => {
+    try {
+      setIsRefreshing(true);
+
+      const listOfTodos = await getTodosListUseCase({ done: false });
+      setData(listOfTodos);
+    } finally {
+      setIsRefreshing(false);
+    }
+  }, []);
+
+  const onDone = useCallback(
+    async (id: string) => {
+      await markAsDoneUseCase({ todoId: id });
+      refreshData();
+    },
+    [refreshData]
+  );
+
+  const onCreate = useCallback(
+    async (description: string) => {
+      try {
+        setIsCreating(true);
+        await createTodoUseCase({ description });
+        refreshData();
+      } finally {
+        setIsCreating(false);
+      }
+    },
+    [refreshData]
+  );
+
+  const renderItem = useCallback(
+    ({ item }: ListRenderItemInfo<Todo>) => {
+      return <TodoItem data={item} onDone={() => onDone(item.id)} />;
+    },
+    [onDone]
+  );
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text>Default Screen</Text>
-    </View>
+    <Container>
+      <Create loading={isCreating} onCreate={onCreate} />
+
+      <List
+        data={data}
+        keyExtractor={item => `default-todo-${item.id}`}
+        renderItem={renderItem}
+        refreshing={isRefreshing}
+        onRefresh={refreshData}
+        ListHeaderComponent={<Title>Lista de tarefas</Title>}
+        ListEmptyComponent={<Empty />}
+      />
+    </Container>
   );
 }
---
+++ src/screens/Default/Main/styles.ts
@@ -0,0 +1,22 @@
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+
+  padding: 20px 0;
+`;
+
+export const List = styled.FlatList.attrs({
+  contentContainerStyle: {
+    padding: 20
+  },
+})`` as React.ComponentType as new <T>() => FlatList<T>;
+
+export const Title = styled.Text`
+  color: #026FF9;
+  font-size: 18px;
+  font-weight: bold;
+  
+  margin-bottom: 20px;
+`;
