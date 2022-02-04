59da8db6c23e6aae75843c8f2da27bcf57e79496
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-02-04 07:14:45 -0300

    add project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { AppNavigation } from './routes/AppNavigation';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <AppNavigation />
+    <QueryClientProvider client={queryClient}>
+      <AppNavigation />
+    </QueryClientProvider>
   );
 }
